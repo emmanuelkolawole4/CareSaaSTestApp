d8b110f71b3e3f477e16725c9e01d8372e85063b
Folahanmi Kolawole <emmanuelkolawole4@gmail.com>
2024-06-16 10:56:06 +0100

    Initial Commit

---
+++ CareSaaSTestApp/CareSaaSTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareSaaSTestAppApp.swift
+//  CareSaaSTestApp
+//
+//  Created by MacBook on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CareSaaSTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
